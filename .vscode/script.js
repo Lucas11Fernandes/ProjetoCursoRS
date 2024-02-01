974cd26dba913bbf13e3699a866446dcdf759909
Lucas Fernandes <129237571+devLucasFernandes11@users.noreply.github.com>
2024-02-01 11:26:40 -0300

    Animações com CSS

--- .vscode/script.js
+++ .vscode/script.js
@@ -5,7 +5,7 @@ function toggleMode(){
     const img = document.querySelector("#profile img")
 
     if(html.classList.contains('light')) {
-       img.setAttribute("src", "./assets/user-avatar_-320x340.png")
+       img.setAttribute("src", "./assets/6596121.png")
      } else {
        img.setAttribute("src", "./assets/6596121.png")
      }
